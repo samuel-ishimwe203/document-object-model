d8bd3273aa8624ea54d379c9a3a35a185752fa6a
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-07-07 18:30:49 +0200

    updation

--- quizApp/quizApp.js
+++ quizApp/quizApp.js
@@ -41,6 +41,7 @@ const quizData= [
 
 let questions= [...quizData].sort(()=>Math.random()-0.5);
 let currentQuestion= 0;
+let score=0;
 
 const questionElement=document.getElementById('question');
 const optionsElement=document.getElementById('options');
@@ -52,17 +53,61 @@ function loadQuestion(){
     
     const qus=questions[currentQuestion];
      questionElement.textContent=`Q${currentQuestion +1}. ${qus.question}`;
-
+    optionsElement.innerHTML="";
    
 
     qus.options.forEach((option,index)=>{
         const btn =document.createElement('button');
         btn.classList.add('option-btn')
         btn.textContent=option;
+        btn.addEventListener('click',()=> selectAnswer(index))
         optionsElement.appendChild(btn)
     });
-
+      
+    nextBtn.style.display="none";
      
+}
+
+function selectAnswer(index){
+    const qus= questions[currentQuestion]
+     const buttons =document.querySelectorAll('.option-btn')
+    buttons.forEach(btn=>btn.disabled= true)
+     if(index ===qus.correct){
+        score++;
+        buttons[index].classList.add("correct")
+
+    }
+    else{
+         buttons[index].classList.add("wrong")
+         buttons[qus.correct].classList.add("correct")
+    }
+
+    nextBtn.style.display="inline-block";
+}
+
+nextBtn.addEventListener('click',()=>{
+   currentQuestion++;
+    
+   if(currentQuestion<questions.length){
+    loadQuestion()
+   }  
+   else{
+    showResult();
+      
+   }
+})
+function showResult(){
+    nextBtn.style.display="none";
+    const highScore=localStorage.getItem("quizHighScore")|| 0;
+    const isNew= score>highScore;
+    if(isNew){
+        localStorage.setItem("quizHighScore",score)
+    }
+     resultEl.innerHTML=` <h3>Wow!!! Quiz complited<h3> 
+     <p> You have score ${score} out of ${questions.length} questions </p>
+     <p>Highest Score : ${Math.max(score,highScore)}</p>
+     ${isNew ?"<p>Hey,New High Score!</p>":""}
+     <button onclick="location.reload()"> Restart Quiz</button>`    
 }
 
 loadQuestion()
